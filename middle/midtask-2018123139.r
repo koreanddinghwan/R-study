3cb042d9bd3ded2ee74d1bdae1eba1f9880390cd
myukang <myunghwan0421@gmail.com>
2022-11-04 14:18:50 +0900

    FEAT:SUBMIT10weeks

--- middle/midtask-2018123139.r
+++ middle/midtask-2018123139.r
@@ -60,7 +60,6 @@ dim(df_ms)
 summary(df_ms)
 #각 속성별로 요약데이터를 얻을 수 있다.
 #몸무게데이터, 뇌용량 데이터의 최소값과 최대값 차이가 크며.
-#렘수면, 수면사이클, 뇌용량 데이터의 결측치가 존재하는 것을 알 수 있다.
 #특히 수면사이클 결측치가 전체 데이터83개의 절반 이상이다.
 
 #2.1 모든 변수에 대하여 각각 결측치가 있는 변수인지 확인하세요.
@@ -85,7 +84,8 @@ table(is.na(df_ms$bodywt))
 #summary(df_ms)
 #name, genus, vore, order, conservation
 
-# 칼럼값에 대해 결측치를 Unknown으로 바꾸하는 함수
+# 칼럼값에 대해 결측치를 Unknown으로 바꾸하는 사용자 정의 함수
+# 사용자 정의함수 배웠으니, 사용하겠습니다.
 fnc_na_to_unknown <- function(origin) {
 	changed <- ifelse(is.na(origin), "Unknown", origin)
 	return(changed)
@@ -122,9 +122,9 @@ boxplot(df_ms$bodywt)#존재
     #나머지 극단치가 있는 변수에 대해서 결측치 처리를 한 뒤, 결측치의 수를 확인하세요.
 
 #sleep_cycle, sleep_rem
-#결측치 처리 함수 정의
+#결측치 처리 사용자 정의 함수
 #boxplot->stats 사용
-convert_extreme_na <- function(variable){
+fnc_extreme_to_na <- function(variable){
 	list_boxplot_stats <- boxplot(variable)$stats
 	value_min <- list_boxplot_stats[1, ]
 	value_max <- list_boxplot_stats[5, ]
@@ -134,12 +134,13 @@ convert_extreme_na <- function(variable){
 	return(variable)
 }
 
-df_ms$sleep_cycle <- convert_extreme_na(df_ms$sleep_cycle)
+df_ms$sleep_cycle <- fnc_extreme_to_na(df_ms$sleep_cycle)
 table(is.na(df_ms$sleep_cycle))
 
-df_ms$sleep_rem <- convert_extreme_na(df_ms$sleep_rem)
+df_ms$sleep_rem <- fnc_extreme_to_na(df_ms$sleep_rem)
 table(is.na(df_ms$sleep_rem))
 
+#극단치를 벗어난 값들이 결측치화되었다.
 
 #3.1 총수면량에서 렘(rem)수면량을 제외한 비렘수면량(sleep_nonrem)을 파생변수로 만들어 보세요.
 
@@ -186,7 +187,7 @@ df_ms <- df_ms %>%
 			df_ms$sleep_nonrem * 60)
 	)
 
-df_ms
+#names(df_ms)
 
 #4.1 식성 구분(육/잡/초/충/모름)이 들어 있는 변수의 각 식성별 빈도수를 빈도표로 확인해보세요.
 
@@ -199,6 +200,7 @@ table(df_ms$vore)
 df_ms[df_ms$vore == "omni",][c("sleep_total_min",
 							   "sleep_rem_min",
 							   "sleep_nonrem_min")]
+
 
 df_ms %>%
 	filter(vore == "omni") %>%
@@ -224,6 +226,12 @@ df_ms %>%
 #4.4 식성 구분(육/잡/초/충/모름)별 총수면량(분 단위)을 막대그래프로 나타내보세요. 
     #이때 막대그래프는 평균이 높은 순으로 보여지게 하세요.
 
+#따로 데이터프레임을 정의해서 사용할수도 있지만, 
+#메모리 상에 굳이 남아있을 필요가 없을 것 같아 확인만하고, ggplot에 바로 넣어주겠습니다.
+#df_ms %>%
+#		group_by(vore) %>%
+#		summarise(mean_sleep_total_min = mean(sleep_total_min))
+
 ggplot(
 	data = df_ms %>%
 		group_by(vore) %>%
@@ -237,8 +245,8 @@ ggplot(
     #red_list라는 이름의 데 이터 프레임으로 만들어보세요.
 
 red_list <- data.frame(
-					conservation = c("en", "vu", "nt", "cd", "lc", "domesticated"),
-					risk = c("high", "high", "low", "low", "low", "non")
+				conservation = c("en", "vu", "nt", "cd", "lc", "domesticated"),
+				risk = c("high", "high", "low", "low", "low", "non")
 )
 
 #red_list
@@ -307,13 +315,13 @@ df_ms <- df_ms %>%
 
 #7.1 제공된 mammal_theria CSV파일을 불러오세요. 
     #(변수명 일부가 깨져서 나온다면 fileEncoding="UTF-8-BOM" 파라미터를 설정하세요.)
-
+#header = TRUE옵션 지정.
 df_mth <- read.csv("mammal_theria.csv", header = TRUE)
 
 #7.2 df_ms 데이터에 두 분류 칼럼 theria_main와 theria_sub이 오른쪽에 파생변수로 추가되도록 하세요.
 
 #table(is.na(df_ms$order))
-#결측치 없으므로, order를 key값으로 join
+#결측치 없으므로, inner_join 써도되나, order를 key값으로 join
 df_ms <- left_join(df_ms, df_mth, by = "order")
 
 #7.3 로라시아상목(Laurasiatheria)과 영장상목(Euarchontoglires)만 추출하여 
@@ -333,12 +341,13 @@ df_ms %>%
     #각각 몇 동물씩 있는지 빈도표를 확인해보세요.
 
 df_ms <- df_ms %>%
-	mutate(sleep_grade = ifelse(df_ms$sleep_total >= 15, "A",
-							ifelse(df_ms$sleep_total >= 10, "B", 
-								ifelse(df_ms$sleep_total >= 5, "C",
-									"D")
-								)
-							)
+	mutate(sleep_grade = 
+			ifelse(df_ms$sleep_total >= 15, "A",
+			ifelse(df_ms$sleep_total >= 10, "B", 
+			ifelse(df_ms$sleep_total >= 5, "C",
+				"D")
+				)
+			)
 	)
 
 #table함수로 빈도표 확인
@@ -350,23 +359,31 @@ table(df_ms$sleep_grade)
 #table(is.na(df_ms$sleep_nonrem))
 #table(is.na(df_ms$sleep_total))
 
+#비렘수면 백분율 계산하는 사용자정의함수
 get_nonrem_ratio <- function(sleep_total, sleep_nonrem) {
 	return((sleep_nonrem / sleep_total) * 100)
 }
 
 #sleep_nonrem 결측치 존재하므로, ifelse사용
 df_ms <- df_ms %>%
-	mutate(nonrem_ratio = ifelse(is.na(df_ms$sleep_nonrem),
-								 NA,
-								 get_nonrem_ratio(df_ms$sleep_total, df_ms$sleep_nonrem)))
+	mutate(nonrem_ratio =
+			ifelse(is.na(df_ms$sleep_nonrem),
+				NA,
+				get_nonrem_ratio(df_ms$sleep_total, df_ms$sleep_nonrem)
+				)
+	)
 
-View(df_ms)
 #8.3 수면량 등급별 비렘수면 백분율을 막대그래프로 나타내보세요. 
     #이때 막대그래프는 평균이 낮은 순으로 보여지게 하세요.
 
 #?geom_col()
 #table(is.na(df_ms$nonrem_ratio))
+#상동의 사유로 ggplot2 내부에서 만들어주겠습니다.
 #group_by sleep_grade해서 nonrem_ration를 mean_nonrem으로 구한다.
+#df_ms %>%
+#		group_by(sleep_grade) %>%
+#		summarise(mean_nonrem = mean(nonrem_ratio, na.rm = TRUE))
+
 ggplot(
 	data = df_ms %>%
 		group_by(sleep_grade) %>%
@@ -377,6 +394,10 @@ ggplot(
 #8.4 식성 구분(육/잡/초/충/모름)별 비렘수면 백분율을 막대그래프로 나타내보세요. 
     #이때 막대그래프 는 평균이 낮은 순으로 보여지게 하세요.
 #names(df_ms)
+#상동의 사유로 ggplot2 내부에서 만들어주겠습니다.
+#	data = df_ms %>%
+#		group_by(vore) %>%
+#		summarise(mean_nonrem = mean(nonrem_ratio, na.rm = TRUE)),
 
 ggplot(
 	data = df_ms %>%
@@ -398,10 +419,10 @@ human_brainwt
 
 df_ms <- df_ms %>%
 	mutate(brain_grade =
-					ifelse(is.na(df_ms$brain_ratio), NA,
-						ifelse(df_ms$brain_ratio > human_brainwt, "High",
-							ifelse(df_ms$brain_ratio == human_brainwt, "Human",
-								"Low")))
+				ifelse(is.na(df_ms$brain_ratio), NA,
+					ifelse(df_ms$brain_ratio > human_brainwt, "High",
+						ifelse(df_ms$brain_ratio == human_brainwt, "Human",
+							"Low")))
 )
 
 df_ms %>%
